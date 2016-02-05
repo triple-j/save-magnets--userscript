bcce4adb3575c0746a521883f979e3451afeed74
Jeremie Jarosh <jeremie@jarosh.org>
2016-02-04 21:58:53 -0600

    Re-code For Better Firefox Support

--- savemagnets.user.js
+++ savemagnets.user.js
@@ -3,25 +3,9 @@
 // @author        Jeremie Jarosh
 // @namespace     https://github.com/triple-j
 // @description   Save Magnet links as text files.
-// @version       1.2
+// @version       1.3
 // @include       *
 // ==/UserScript==
-
-// saveData from: http://jsfiddle.net/koldev/cw7w5/
-var saveData = (function () {
-	var a = document.createElement("a");
-	document.body.appendChild(a);
-	a.style = "display: none";
-	return function (data, fileName, fileType) {
-		var fileType = fileType || "text/plain",
-			blob = new Blob([data], {type: fileType}),
-			url = window.URL.createObjectURL(blob);
-		a.href = url;
-		a.download = fileName;
-		a.click();
-		window.URL.revokeObjectURL(url);
-	};
-}());
 
 var links = document.querySelectorAll('a[href^="magnet:"]');
 
@@ -30,8 +14,17 @@ for ( var i=0; i < links.length; i++ ) {
 		evt.preventDefault();
 		
 		var magnetText = this.getAttribute('href'),
-			filename = (this.textContent.trim() || "download") + ".magnet";
+			fileNameToSaveAs = (this.textContent.trim() || "download") + ".magnet",
+			textFileAsBlob = new Blob([magnetText], {type: "text/plain"}),
+			downloadLink = document.createElement("a");
 		
-		saveData( magnetText, filename, "text/plain" );
+		downloadLink.download = fileNameToSaveAs;
+		downloadLink.innerHTML = "Download Magnet File";
+		downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+		downloadLink.onclick = function(event) { document.body.removeChild(event.target); };
+		downloadLink.style.display = "none";
+		document.body.appendChild(downloadLink);
+		
+		downloadLink.click();
 	});
 }
